747050dd8bc3c599774a2e90300b5a42aefaffb0
Manav Mahesh Sanger <97792622+karnop@users.noreply.github.com>
2023-11-12 13:30:06 +0530

    Time: 3 ms (39.03%), Space: 39.9 MB (35.15%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
@@ -1,34 +1,27 @@
 class Solution {
 
-    public void subsets(int[] nums, int index, ArrayList<Integer> list, ArrayList<ArrayList<Integer>> sub)
+    public int subsets(int[] nums, int index, ArrayList<Integer> list, ArrayList<ArrayList<Integer>> sub)
     {
         if(index == nums.length)
-        {
-            ArrayList<Integer> temp = new ArrayList<>(list);
-            sub.add(temp);
-            return;
+        {   
+            int subxor = 0;
+            for(int i=0; i<list.size(); i++)
+            {
+                subxor = (subxor^list.get(i));
+            }
+            return subxor;
         }
 
         list.add(nums[index]);
-        subsets(nums, index+1, list, sub);
+        int sum1 = subsets(nums, index+1, list, sub);
         list.remove(list.size() - 1);
-        subsets(nums, index+1, list, sub);
+        int sum2 = subsets(nums, index+1, list, sub);
+        return sum1 + sum2;
     } 
     public int subsetXORSum(int[] nums) 
     {
         ArrayList<Integer> list = new ArrayList<>();
         ArrayList<ArrayList<Integer>> sub = new ArrayList<>();
-        subsets(nums, 0, list, sub);
-        int res = 0;
-        for(ArrayList<Integer> l : sub)
-        {
-            int subxor = 0;
-            for(int i=0; i<l.size(); i++)
-            {
-                subxor = (subxor^l.get(i));
-            }
-            res+=subxor;
-        }
-        return res;
+        return subsets(nums, 0, list, sub);
     }
 }
