87c285c18ba29739610870b11c7144a7d81b5324
Manav Mahesh Sanger <97792622+karnop@users.noreply.github.com>
2025-02-13 21:11:34 +0530

    Time: 8 ms (9.34%), Space: 21.8 MB (14.79%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int helper(int ind, int buy, vector<int>& prices, vector<vector<int>> &dp) {
+        // note : in dp stock problems always move in forward direction not in reverse 
+        if (ind == prices.size()) {
+            return 0;
+        }
+        if (dp[ind][buy] != -1) return dp[ind][buy];
+
+        if (buy == 0) {
+            return dp[ind][buy] = max(helper(ind+1, 1, prices, dp) - prices[ind], helper(ind+1, 0, prices, dp));
+        } else {
+            return dp[ind][buy] = max(helper(ind+1, 0, prices, dp) + prices[ind], helper(ind+1, 1, prices, dp));
+        }
+    }
+
+    int helperTabulation(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n+1, vector<int>(2, 0));
+        int ind = n;
+        dp[ind][0] = 0;
+        dp[ind][1] = 0;
+
+        for(ind = n-1; ind>=0; ind--) {
+            for(int buy = 1; buy >= 0; buy--) {
+                if (buy == 0) {
+                    dp[ind][buy] = max(dp[ind+1][1] - prices[ind], dp[ind+1][0]);
+                } else {
+                    dp[ind][buy] = max(dp[ind+1][0] + prices[ind], dp[ind+1][1]);
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n+1, vector<int>(2, -1));
+        // return helper(n, 0, prices, dp);  // Start with not holding stock
+        return helperTabulation(prices);
+    }
+};
