335fbfe66dcbca536d832fca16fc776173f724d5
Manav Mahesh Sanger <97792622+karnop@users.noreply.github.com>
2025-03-01 11:55:57 +0530

    Time: 0 ms (100%), Space: 13 MB (87.59%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -3,20 +3,25 @@ public:
     vector<int> applyOperations(vector<int>& nums) {
         int n = nums.size();
         int zeros = 0;
-        vector<int> ans;
         for(int i=0; i<n-1; i++) {
-            if(nums[i] == 0) {
-                zeros++;
-                continue;
-            }
-            else if(nums[i] == nums[i+1]) {
+           if(nums[i] == nums[i+1]) {
                 nums[i] *= 2;
                 nums[i+1] = 0;
-            }
-            ans.push_back(nums[i]);
+           }
         }
-        ans.push_back(nums[n-1]);
-        for(int i=0; i<zeros; i++) ans.push_back(0);
-        return ans;
+        
+        // 2 pointer approach
+        int index = 0;
+        for(int i=0; i<n; i++) {
+            if(nums[i] != 0) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        while(index < n) {
+            nums[index] = 0;
+            index++;
+        }
+        return nums;
     }
 };
