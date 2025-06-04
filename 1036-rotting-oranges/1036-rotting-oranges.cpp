b07885afa775cf447d59550c9cb040c1e903eabf
Manav Mahesh Sanger <97792622+karnop@users.noreply.github.com>
2025-06-04 19:22:41 +0530

    Time: 0 ms (100%), Space: 16.5 MB (97.49%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,57 +1,47 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        // making a duplicate copy of data
         int rows = grid.size();
         int cols = grid[0].size();
-        vector<vector<int>> data = grid;
 
-        int freshOranges = 0;
-        // {{r,c}, t}
-        queue<pair<pair<int, int>, int>>q;
+        int time = 0;
+        int fresh = 0;
 
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<cols; j++){
-                int val = grid[i][j];
-                data[i][j] = val;
-                if(val == 1) freshOranges++;
-                if(val == 2) {
-                    q.push({{i, j}, 0});
+        // traversal
+        queue<pair<int , int>> q;
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<cols; j++) {
+                if(grid[i][j] == 2) {
+                    q.push({i, j});
+                }
+                if(grid[i][j] == 1) fresh++;
+            }
+        }
+
+        while(!q.empty() && fresh > 0) {
+            time++;
+            int n = q.size();
+
+            for(int j=0; j<n; j++) {
+                int x = q.front().first;
+                int y = q.front().second;
+                q.pop();
+
+                int dx[] = {0, 0, 1, -1};
+                int dy[] = {-1, 1, 0, 0};
+
+                for(int i=0; i<4; i++) {
+                    int r = x + dx[i];
+                    int c = y + dy[i];
+                    if(r >=0 && r < rows && c >= 0 && c < cols && grid[r][c] == 1) {
+                        grid[r][c] = 2;
+                        q.push({r, c});
+                        fresh--;
+                    } 
                 }
             }
         }
 
-        // queue logic
-        int maxtime = 0;
-        int queCnt = 0;
-
-        while(!q.empty()){
-            auto elem = q.front();
-            int r = elem.first.first;
-            int c = elem.first.second;
-            int t = elem.second;
-            q.pop();
-
-            maxtime = max(maxtime, t);
-
-            // insertions
-            int delrow[] = {-1, 0, 1, 0};
-            int delcol[] = {0, 1, 0, -1};
-
-            for(int i=0; i<4; i++){
-                int nrow = r+delrow[i];
-                int ncol = c+delcol[i];
-
-                if(nrow >=0 && nrow < rows && ncol >= 0 && ncol < cols && data[nrow][ncol] == 1 ){
-                    data[nrow][ncol] = 2;
-                    q.push({{nrow, ncol}, t+1});
-                    queCnt++;
-                }   
-            }
-        }
-
-        if(queCnt != freshOranges) return -1;
-        return maxtime;
-
+        return fresh == 0 ? time : -1;
     }
 };
